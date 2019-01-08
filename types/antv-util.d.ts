fba92a549f44de837d8e6c5a54091c4dc1ed202f
Alcyone <823289099@qq.com>
2019-01-08 17:51:21 +0800

    util定义完善

--- types/antv-util.d.ts
+++ types/antv-util.d.ts
@@ -1,128 +1,133 @@
 declare module '@antv/util/lib/type/is-function' {
-  const isFunction: any;
-  export = isFunction
+  const isFunction: (value: any) => boolean;
+  export = isFunction;
 }
-declare module '@antv/util/lib/type/is-object'{
-  const isObject: any;
+declare module '@antv/util/lib/type/is-object' {
+  const isObject: (value: any) => boolean;
   export = isObject;
 }
-declare module '@antv/util/lib/type/is-boolean'{
-  const isBoolean: any;
+declare module '@antv/util/lib/type/is-boolean' {
+  const isBoolean: (value: any) => boolean;
   export = isBoolean;
 }
-declare module '@antv/util/lib/type/is-nil'{
-  const isNil: any;
+declare module '@antv/util/lib/type/is-nil' {
+  const isNil: (value: any) => boolean;
   export = isNil;
 }
-declare module '@antv/util/lib/type/is-string'{
-  const isString: any;
+declare module '@antv/util/lib/type/is-string' {
+  const isString: (value: any) => boolean;
   export = isString;
 }
-declare module '@antv/util/lib/type/is-array'{
-  const isArray: any;
+declare module '@antv/util/lib/type/is-array' {
+  const isArray: (value: any) => boolean;
   export = isArray;
 }
-declare module '@antv/util/lib/type/is-number'{
-  const isNumber: any;
+declare module '@antv/util/lib/type/is-number' {
+  const isNumber: (value: any) => boolean;
   export = isNumber;
 }
-declare module '@antv/util/lib/is-empty'{
-  const isEmpty: any;
+declare module '@antv/util/lib/is-empty' {
+  const isEmpty: (value: any) => boolean;
   export = isEmpty;
 }
-declare module '@antv/util/lib/unique-id'{
-  const uniqueId: any;
+declare module '@antv/util/lib/unique-id' {
+  const uniqueId: (prefix?: string) => string;
   export = uniqueId;
 }
-declare module '@antv/util/lib/clone'{
-  const clone: any;
+declare module '@antv/util/lib/clone' {
+  const clone: <T>(obj: T) => T;
   export = clone;
 }
-declare module '@antv/util/lib/deep-mix'{
-  const deepMix: <T, U>(dist: T , src: U, level: number, max: number) => T & U;
+declare module '@antv/util/lib/deep-mix' {
+  const deepMix: <T, U>(dist: T, src: U, level: number, max: number) => T & U;
   export = deepMix;
 }
-declare module '@antv/util/lib/mix'{
-  const mix: any;
+declare module '@antv/util/lib/mix' {
+  const mix: (dist: any, src1?: any, src2?: any, src3?: any) => any;
   export = mix;
 }
 // declare module '@antv/util/lib/deep-mix'{
 //   const merge: <T, U>(dist: T, src: U, lever: number, max: number) => T & U;
 //   export = merge;
 // }
-declare module '@antv/util/lib/string/upper-first'{
-  const upperFirst: any;
+declare module '@antv/util/lib/string/upper-first' {
+  const upperFirst: (value: string) => string;
   export = upperFirst;
 }
-declare module '@antv/util/lib/each'{
-  const each: any;
+declare module '@antv/util/lib/each' {
+  const each: (elements: any, func: any) => void;
   export = each;
 }
-declare module '@antv/util/lib/is-equal'{
-  const isEqual: any;
+declare module '@antv/util/lib/is-equal' {
+  const isEqual: (value: any, other: any) => boolean;
   export = isEqual;
 }
-declare module '@antv/util/lib/to-array'{
-  const toArray: any;
+declare module '@antv/util/lib/to-array' {
+  const toArray: (value: any) => any[];
   export = toArray;
 }
-declare module '@antv/util/lib/extend'{
-  const extend: any;
+declare module '@antv/util/lib/extend' {
+  const extend: (
+    subclass: any,
+    superclass: any,
+    overrides: any,
+    staticOverrides: any,
+  ) => any;
   export = extend;
 }
-declare module '@antv/util/lib/augment'{
-  const augment: any;
+declare module '@antv/util/lib/augment' {
+  const augment: (c: any, ...args: any[]) => void;
   export = augment;
 }
-declare module '@antv/util/lib/array/pull'{
-  const pull: any;
+declare module '@antv/util/lib/array/pull' {
+  const pull: (arr: any, ...args: any[]) => any;
   export = pull;
 }
-declare module '@antv/util/lib/math/is-number-equal'{
+declare module '@antv/util/lib/math/is-number-equal' {
   const isNumberEqual: (a: number, b: number) => boolean;
   export = isNumberEqual;
 }
-declare module '@antv/util/lib/math/to-radian'{
+declare module '@antv/util/lib/math/to-radian' {
   const toRadian: (degree: number) => number;
   export = toRadian;
 }
-declare module '@antv/util/lib/math/to-degree'{
+declare module '@antv/util/lib/math/to-degree' {
   const toDegree: (radian: number) => number;
   export = toDegree;
 }
-declare module '@antv/util/lib/math/mod'{
+declare module '@antv/util/lib/math/mod' {
   const mod: (n: number, m: number) => number;
   export = mod;
 }
-declare module '@antv/util/lib/math/clamp'{
+declare module '@antv/util/lib/math/clamp' {
   const clamp: (a: number, min: number, max: number) => number;
   export = clamp;
 }
-declare module '@antv/util/lib/dom/create-dom'{
+declare module '@antv/util/lib/dom/create-dom' {
   const createDom: (domStr: any) => any;
   export = createDom;
 }
-declare module '@antv/util/lib/dom/modify-css'{
+declare module '@antv/util/lib/dom/modify-css' {
   const modifyCSS: (dom: any, css: any) => any;
   export = modifyCSS;
 }
-declare module '@antv/util/lib/dom/request-animation-frame'{
+declare module '@antv/util/lib/dom/request-animation-frame' {
   const requestAnimationFrame: (fn: any) => any;
   export = requestAnimationFrame;
 }
-declare module '@antv/util/lib/matrix/mat3'{
+declare module '@antv/util/lib/matrix/mat3' {
   const mat3: any;
   export = mat3;
 }
-declare module '@antv/util/lib/matrix/vec2'{
+declare module '@antv/util/lib/matrix/vec2' {
   const vec2: any;
   export = vec2;
 }
-declare module '@antv/util/lib/matrix/vec3'{
+declare module '@antv/util/lib/matrix/vec3' {
   const vec3: any;
   export = vec3;
 }
-declare module '@antv/util/lib/matrix/transform'{
+declare module '@antv/util/lib/matrix/transform' {
   const transform: (m: any, ts: any) => any;
   export = transform;
 }
