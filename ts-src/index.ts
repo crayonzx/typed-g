04ea3d1751186a84b3ad7561d1f3c429d4ff7a34
Harpes <823289099@qq.com>
2019-01-17 17:47:30 +0800

    添加两个常用类型

--- ts-src/index.ts
+++ ts-src/index.ts
@@ -1,3 +1,41 @@
+namespace G {
+  export interface Style {
+    /**  设置用于填充绘画的颜色、渐变或模式 */
+    fill: string;
+    fillOpacity: number;
+    /**  设置用于笔触的颜色、渐变或模式 */
+    stroke: string;
+    /**  设置用于阴影的颜色 */
+    shadowColor: string;
+    /**  设置用于阴影的模糊级别 */
+    shadowBlur: number;
+    /**  设置阴影距形状的水平距离 */
+    shadowOffsetX: number;
+    /**  设置阴影距形状的垂直距离 */
+    shadowOffsetY: number;
+    /**  设置绘图的当前 alpha 或透明值 */
+    opacity: number;
+    /**  设置新图像如何绘制到已有的图像上 */
+    globalCompositeOperation: string;
+
+    lineAppendWidth: number;
+
+    /** 线型 */
+    lineDash: number | number[];
+  }
+
+  export interface BBox {
+    centerX?: number;
+    centerY?: number;
+    height: number;
+    maxX: number;
+    maxY: number;
+    minX: number;
+    minY: number;
+    width: number;
+  }
+}
+
 const G: {
   Canvas: typeof import('./canvas');
   Group: typeof import('./core/group');
