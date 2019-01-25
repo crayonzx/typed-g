f5a19c420df24fc7f6b762742b598815cc13e4c4
crayon <crayon.zx@gmail.com>
2019-01-25 18:00:19 +0800

    Revert changes to G namepace

--- ts-src/index.ts
+++ ts-src/index.ts
@@ -1,56 +1,29 @@
-const G0: G = {
-  Canvas: require('./canvas'),
-  Group: require('./core/group'),
-  Shape: require('./core/shape'),
-  Arc: require('./shapes/arc'),
-  Circle: require('./shapes/circle'),
-  Dom: require('./shapes/dom'),
-  Ellipse: require('./shapes/ellipse'),
-  Fan: require('./shapes/fan'),
-  Image: require('./shapes/image'),
-  Line: require('./shapes/line'),
-  Marker: require('./shapes/marker'),
-  Path: require('./shapes/path'),
-  Polygon: require('./shapes/polygon'),
-  Polyline: require('./shapes/polyline'),
-  Rect: require('./shapes/rect'),
-  Text: require('./shapes/text'),
-  PathSegment: require('./shapes/util/path-segment'),
-  PathUtil: require('./util/path'),
-  Event: require('./event'),
-  EventEmitter: require('./core/event-emitter'),
-  // version, etc.
-  version: '3.4.0-beta.3'
-};
-
 import CommonNS from './common-ns';
 import ShapesNS from './shapes-ns';
-
-interface G {
-  Canvas: typeof import('./canvas');
-  Group: typeof import('./core/group');
-  Shape: typeof import('./core/shape');
-  Arc: typeof import('./shapes/arc');
-  Circle: typeof import('./shapes/circle');
-  Dom: typeof import('./shapes/dom');
-  Ellipse: typeof import('./shapes/ellipse');
-  Fan: typeof import('./shapes/fan');
-  Image: typeof import('./shapes/image');
-  Line: typeof import('./shapes/line');
-  Marker: typeof import('./shapes/marker');
-  Path: typeof import('./shapes/path');
-  Polygon: typeof import('./shapes/polygon');
-  Polyline: typeof import('./shapes/polyline');
-  Rect: typeof import('./shapes/rect');
-  Text: typeof import('./shapes/text');
-  PathSegment: typeof import('./shapes/util/path-segment');
-  PathUtil: typeof import('./util/path');
-  Event: typeof import('./event');
-  EventEmitter: typeof import('./core/event-emitter');
-  version: string;
-}
-
 namespace G {
+  export const Canvas: typeof import('./canvas') = require('./canvas');
+  export const Group: typeof import('./core/group') = require('./core/group');
+  export const Shape: typeof import('./core/shape') = require('./core/shape');
+  export const Arc: typeof import('./shapes/arc') = require('./shapes/arc');
+  export const Circle: typeof import('./shapes/circle') = require('./shapes/circle');
+  export const Dom: typeof import('./shapes/dom') = require('./shapes/dom');
+  export const Ellipse: typeof import('./shapes/ellipse') = require('./shapes/ellipse');
+  export const Fan: typeof import('./shapes/fan') = require('./shapes/fan');
+  export const Image: typeof import('./shapes/image') = require('./shapes/image');
+  export const Line: typeof import('./shapes/line') = require('./shapes/line');
+  export const Marker: typeof import('./shapes/marker') = require('./shapes/marker');
+  export const Path: typeof import('./shapes/path') = require('./shapes/path');
+  export const Polygon: typeof import('./shapes/polygon') = require('./shapes/polygon');
+  export const Polyline: typeof import('./shapes/polyline') = require('./shapes/polyline');
+  export const Rect: typeof import('./shapes/rect') = require('./shapes/rect');
+  export const Text: typeof import('./shapes/text') = require('./shapes/text');
+  export const PathSegment: typeof import('./shapes/util/path-segment') = require('./shapes/util/path-segment');
+  export const PathUtil: typeof import('./util/path') = require('./util/path');
+  export const Event: typeof import('./event') = require('./event');
+  export const EventEmitter: typeof import('./core/event-emitter') = require('./core/event-emitter');
+  // version, etc.
+  export const version = '3.4.0-beta.3';
+
   export import Common = CommonNS;
   export import Shapes = ShapesNS;
 
@@ -60,6 +33,4 @@ namespace G {
   export namespace Group {}
   export type Group = import('./core/group');
 }
-
-Object.assign(G, G0);
 export = G;
