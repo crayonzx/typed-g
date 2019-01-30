03ac1633a7ee36973aaaf1d31e8a2a722e00594e
crayon <crayon.zx@gmail.com>
2019-01-30 11:08:52 +0800

    修复无法通过扩充ShapeEx来扩充Shape

--- ts-src/core/shape-ex.ts
+++ ts-src/core/shape-ex.ts
@@ -1,18 +1,6 @@
 /**
  * To augment Shape, augment ShapeEx.
- *
- * TODO: It doesn't work to augment ShapeEx somewhere else for now by:
-```
-  declare module '@antv/g/lib/core/shape-ex' {
-    interface ShapeEx {
-      eventPreFix: string;
-    }
-  }
-```
  */
-import Common from '../common';
-interface ShapeEx {
-  eventPreFix?: string;
-  id?: Common.ID;
+export interface ShapeEx {
 }
 export default ShapeEx;
