01ecbc36c40abf01a3be3aad72f27c8065045417
crayon <crayon.zx@gmail.com>
2019-07-14 11:14:05 +0800

    refine export G

--- ts-src/index.ts
+++ ts-src/index.ts
@@ -1,32 +1,32 @@
+declare namespace G {
+  const Canvas: typeof import('./canvas'),
+  const Group: typeof import('./core/group'),
+  const Shape: typeof import('./core/shape'),
+  const Arc: typeof import('./shapes/arc'),
+  const Circle: typeof import('./shapes/circle'),
+  const Dom: typeof import('./shapes/dom'),
+  const Ellipse: typeof import('./shapes/ellipse'),
+  const Fan: typeof import('./shapes/fan'),
+  const Image: typeof import('./shapes/image'),
+  const Line: typeof import('./shapes/line'),
+  const Marker: typeof import('./shapes/marker'),
+  const Path: typeof import('./shapes/path'),
+  const Polygon: typeof import('./shapes/polygon'),
+  const Polyline: typeof import('./shapes/polyline'),
+  const Rect: typeof import('./shapes/rect'),
+  const Text: typeof import('./shapes/text'),
+  const PathSegment: typeof import('./shapes/util/path-segment'),
+  const PathUtil: typeof import('./util/path'),
+  const Event: typeof import('./event'),
+  const EventEmitter: typeof import('./core/event-emitter'),
+  // version, etc.
+  const version: '3.4.3'
+};
+
 import Common_ from './common';
 import Shape_ from './shape';
 
-namespace G {
-  export const Canvas: typeof import('./canvas') = require('./canvas');
-  export const Group: typeof import('./core/group') = require('./core/group');
-  export const Shape: typeof import('./core/shape') = require('./core/shape');
-  export const Arc: typeof import('./shapes/arc') = require('./shapes/arc');
-  export const Circle: typeof import('./shapes/circle') = require('./shapes/circle');
-  export const Dom: typeof import('./shapes/dom') = require('./shapes/dom');
-  export const Ellipse: typeof import('./shapes/ellipse') = require('./shapes/ellipse');
-  export const Fan: typeof import('./shapes/fan') = require('./shapes/fan');
-  export const Image: typeof import('./shapes/image') = require('./shapes/image');
-  export const Line: typeof import('./shapes/line') = require('./shapes/line');
-  export const Marker: typeof import('./shapes/marker') = require('./shapes/marker');
-  export const Path: typeof import('./shapes/path') = require('./shapes/path');
-  export const Polygon: typeof import('./shapes/polygon') = require('./shapes/polygon');
-  export const Polyline: typeof import('./shapes/polyline') = require('./shapes/polyline');
-  export const Rect: typeof import('./shapes/rect') = require('./shapes/rect');
-  export const Text: typeof import('./shapes/text') = require('./shapes/text');
-  export const PathSegment: typeof import('./shapes/util/path-segment') = require('./shapes/util/path-segment');
-  export const PathUtil: typeof import('./util/path') = require('./util/path');
-  export const Event: typeof import('./event') = require('./event');
-  export const EventEmitter: typeof import('./core/event-emitter') = require('./core/event-emitter');
-  // version, etc.
-  export const version = '3.4.3';
-}
-
-namespace G {
+declare namespace G {
   export import Common = Common_;
   export import Shapes = Shape_;
 
